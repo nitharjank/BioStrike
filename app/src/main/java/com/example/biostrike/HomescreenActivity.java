29573e933e6e5703f7564a1583dcfcbf7ed73268
nkanthasamy3 <46797844+nkanthasamy3@users.noreply.github.com>
2019-11-04 17:26:47 -0500

    Added home screen

---
+++ app/src/main/java/com/example/biostrike/HomescreenActivity.java
@@ -0,0 +1,12 @@
+package com.example.biostrike;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomescreenActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+}

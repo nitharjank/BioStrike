26677e83ecad3fd54ce0ec70f40a29f4dc116ffe
nkanthasamy3 <46797844+nkanthasamy3@users.noreply.github.com>
2020-02-14 14:41:08 -0500

    Added JTDS and the connection

--- app/src/main/java/com/example/biostrike/MainActivity.java
+++ app/src/main/java/com/example/biostrike/MainActivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +29,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public Connection connectionclass(){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String connectionURl = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionURl = "jdbc:sqlserver://advancingtechnoloiges1.database.windows.net:1433;database=ATLETECHS DATABASE;user=ralphonse@advancingtechnoloiges1;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            connection = DriverManager.getConnection(connectionURl);
+        }
+        catch (SQLException se) {
+            Log.e("error here 1" , se.getMessage());
+        }
+        catch (ClassNotFoundException e) {
+            Log.e("error here 1" , e.getMessage());
+        }
+        catch (Exception e) {
+            Log.e("error here 1" , e.getMessage());
+        }
+        return connection;
     }
 }

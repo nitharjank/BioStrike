e4be495f638c4dca661968e140a3827cceb366f7
Soha Jiwani <sohajiwani1@gmail.com>
2020-04-05 23:01:59 -0400

    added graphview in

--- app/src/main/java/com/example/biostrike/PastActivity.java
+++ app/src/main/java/com/example/biostrike/PastActivity.java
@@ -5,12 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PastActivity extends AppCompatActivity {
+
+    LineGraphSeries<DataPoint> series;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.past_screen);
+
+        double y, x;
+        x = -5.0;
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        for (int i = 0; i < 500; i++) {
+            x = x + 0.1; //can't go higher than 0.1 bc graph won't look continuous
+            y = 5 * x + 1;
+            series.appendData(new DataPoint(x, y), true, 500);
+        }
+        graph.addSeries(series);
 
         Button homeButton = (Button) findViewById(R.id.homeButton);
         homeButton.setOnClickListener(new View.OnClickListener() {

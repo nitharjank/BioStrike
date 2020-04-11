153d222178c96bd5517efb5dea9b3bb7e8ead9a9
Owen4217 <owen.miller13@gmail.com>
2020-04-11 16:40:35 -0400

    initial display of left hand sample data

--- app/src/main/java/com/example/biostrike/PastActivity.java
+++ app/src/main/java/com/example/biostrike/PastActivity.java
@@ -2,6 +2,7 @@ package com.example.biostrike;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +12,14 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PastActivity extends AppCompatActivity {
 
     LineGraphSeries<DataPoint> series;
@@ -19,14 +28,17 @@ public class PastActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.past_screen);
 
-        double y, x;
-        x = -5.0;
+        readPastSessionData();
+
+        int y, x;
+        x = 0;
         GraphView graph = (GraphView) findViewById(R.id.graph);
         series = new LineGraphSeries<DataPoint>();
-        for (int i = 0; i < 500; i++) {
-            x = x + 0.1; //can't go higher than 0.1 bc graph won't look continuous
-            y = 5 * x + 1;
-            series.appendData(new DataPoint(x, y), true, 500);
+        System.out.println(dataSamples.size());
+        for (int i = 0; i < dataSamples.size(); i++) {
+            x = dataSamples.get(i).getSessionID();
+            y = dataSamples.get(i).getLeftHandStrike();
+            series.appendData(new DataPoint(x, y), true, dataSamples.size());
         }
         graph.addSeries(series);
 
@@ -55,5 +67,32 @@ public class PastActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private List<SampleData> dataSamples = new ArrayList<>();
+    private void readPastSessionData() {
+        InputStream is = getResources().openRawResource(R.raw.past_session);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8")));
+        String line;
+        try {
+            reader.readLine(); // skip headers
+            while( (line = reader.readLine()) != null) {
+                // split by commas
+                String[] splits = line.split(",");
+                // read data
+                SampleData sample = new SampleData();
+                sample.setSessionID(Integer.parseInt(splits[0]));
+                sample.setSesssionDate(splits[1]);
+                sample.setLeftHandStrike(Integer.parseInt(splits[2]));
+                sample.setRightHandStrike(Integer.parseInt(splits[3]));
+                sample.setLeftLegStrike(Integer.parseInt(splits[4]));
+                sample.setRightLegStrike(Integer.parseInt(splits[5]));
+                dataSamples.add(sample);
+            }
+        } catch (IOException e) {
+            Log.wtf("PastActivity", "Error reading past session csv file", e);
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/example/biostrike/SampleData.java
@@ -0,0 +1,58 @@
+package com.example.biostrike;
+
+public class SampleData {
+    private int sessionID;
+    private String sesssionDate;
+    private int leftHandStrike;
+    private int rightHandStrike;
+    private int leftLegStrike;
+    private int rightLegStrike;
+
+    public int getSessionID() {
+        return sessionID;
+    }
+
+    public void setSessionID(int sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    public String getSesssionDate() {
+        return sesssionDate;
+    }
+
+    public void setSesssionDate(String sesssionDate) {
+        this.sesssionDate = sesssionDate;
+    }
+
+    public int getLeftHandStrike() {
+        return leftHandStrike;
+    }
+
+    public void setLeftHandStrike(int leftHandStrike) {
+        this.leftHandStrike = leftHandStrike;
+    }
+
+    public int getRightHandStrike() {
+        return rightHandStrike;
+    }
+
+    public void setRightHandStrike(int rightHandStrike) {
+        this.rightHandStrike = rightHandStrike;
+    }
+
+    public int getLeftLegStrike() {
+        return leftLegStrike;
+    }
+
+    public void setLeftLegStrike(int leftLegStrike) {
+        this.leftLegStrike = leftLegStrike;
+    }
+
+    public int getRightLegStrike() {
+        return rightLegStrike;
+    }
+
+    public void setRightLegStrike(int rightLegStrike) {
+        this.rightLegStrike = rightLegStrike;
+    }
+}
